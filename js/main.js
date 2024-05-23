dd264daf609ac0ca2b83b97eb1dfd91d68889263
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-05-23 10:51:06 -0700

    update html css and js files.

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+    strings: ['Singer', 'Song Writer', 'Composer', 'Music Maker', 'YouTuber', 'Group Performer', 'Frontend Web Developer', 'Web Designer', 'UI/UX Designer', 'Figma Web Designer', 'Responsive Web Designer'],
+    typeSpeed: 116,
+});

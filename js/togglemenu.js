9c5c37639a25af6d9ea9c52c5132aace3399fb8e
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-05-27 03:01:29 -0700

    update html css and js files.

---
+++ js/togglemenu.js
@@ -0,0 +1,8 @@
+menuIcon = document.querySelector('.menu-icon')
+navSection = document.querySelector('.navber-section')
+navberLinks = document.querySelector('.navber-links')
+menuIcon = document.querySelector('.menu-icon')
+
+burger.addEventListner('click', ()=>{
+    menuIcon.classList.toggle
+})

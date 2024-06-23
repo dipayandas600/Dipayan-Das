f11f03fba6dd7aa3bbf5ff8ed4b0250d72cbd57a
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-06-23 10:36:51 -0700

    update html css and js files.

--- js/typed.js
+++ js/typed.js
@@ -1,4 +1,7 @@
 var typed = new Typed('#element', {
-    strings: ['Singer', 'Song Writer', 'Composer', 'Music Maker', 'YouTuber', 'Group Performer', 'Frontend Web Developer', 'Web Designer', 'UI/UX Designer', 'Figma Web Designer', 'Responsive Web Designer'],
+    strings: ['Singer', 'Song Writer', 'Composer', 'Music Maker', 'Editor' , 'YouTuber', 'Group Performer', 'Frontend Web Developer', 'Web Designer', 'UI/UX Designer', 'Figma Web Designer', 'Responsive Web Designer'],
     typeSpeed: 116,
+    backSpeed: 116,
+    backSpeeddeley: 1000,
+    loop: true,
 });

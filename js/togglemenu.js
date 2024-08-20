38b8fb704118898dbf59313bbb77d942d5b0601a
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-08-20 09:27:32 -0700

    update all files.

--- js/togglemenu.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('navber-links');
-
-menuIcon.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
--- js/typed.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed('#element', {
-    strings: ['Singer', 'Song Writer', 'Composer', 'Music Maker', 'Editor' , 'YouTuber', 'Group Performer', 'Frontend Web Developer', 'Web Designer', 'UI/UX Designer', 'Figma Web Designer', 'Responsive Web Designer'],
-    typeSpeed: 116,
-    backSpeed: 116,
-    backSpeeddeley: 1000,
-    loop: true,
-});

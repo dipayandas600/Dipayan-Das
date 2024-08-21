d0ccdec77dc88e5c5d27101afbd7742c47140471
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-08-21 03:38:49 -0700

    update all files.

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navber.css';
+import '../components/Pages/Responsive1.css';
 
 const Navber = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header-section">
         <nav className="navber-section">
             <h1 className="logo">Dipayan Das</h1>
 
-            <ul className="navber-links">
+            <ul className={`navber-links ${isOpen ? 'open' : ''}`}>
                 <li>Home</li>
                 <li>About</li>
                 <li>Skills</li>
@@ -16,14 +24,14 @@ const Navber = () => {
                 <li>Contact</li>
             </ul>
 
-            <div className="hamburger">
+            <div className="hamburger" onClick={toggleMenu}>
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span>
             </div>
         </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Navber

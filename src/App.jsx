06da7f192faabdfab87933021376643a844d771c
Dipayan Das <80384778+dipayandas600@users.noreply.github.com>
2024-08-23 11:05:33 -0700

    update jsx and css files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navber from '../src/components/Navber';
+import Home from '../src/components/Home';
 
 const App = () => {
   return (
-    <Navber/>
+    <>
+      <Navber/>
+      <Home/>
+    </>
   );
 };
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
-// import backgroundimage from '../assets/images/backgroundimage.jpg';
+import './Home.css';
+import image from '../assets/images/image.jpg';
 
 const Home = () => {
   return (
     <div className="home-section">
+      <div className="right-and-left-side">
 
+        <div className="right-side">
+          <h1 className="text-1">
+            Hi________ <br />
+            This Is Dipayan Das
+          </h1>
+
+          <h1 className="text-2">I Am A</h1>
+          
+          <div className="buttons">
+            <button className="hire-me">Hire Me</button>
+            <button className="github">Github</button>
+            <button className="download-cv">Download CV</button>
+          </div>
+        </div>
+        
+        <div className="left-side">
+          <img src={image} width='488px' className='background-image' />
+        </div>
+
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home
